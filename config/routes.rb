8cc29ec9e362f83da090f4f2bcc52f5488f2e399
ichanKnows <eugenio42685@yahoo.com>
2020-04-12 23:05:26 +0800

    finished sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   resources :accounts
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] 
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_085203) do
+ActiveRecord::Schema.define(version: 2020_04_12_132806) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_085203) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
