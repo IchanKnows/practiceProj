da4a28ff50b8cdd196360bf731c7c2d42937db87
ichanKnows <eugenio42685@yahoo.com>
2020-04-12 15:35:36 +0800

    commit on feature branch with new users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_093852) do
+ActiveRecord::Schema.define(version: 2020_04_12_061405) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "motto"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
